e141e98974846bd5751894172459c73df44c2625
Robin Foe <rfoe@Robins-MacBook-Pro.local>
2015-11-11 17:05:33 +0800

    commit

--- .meteor/local/build/programs/web.browser/app/client/templates/template.main-layout.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("mainLayout");
-Template["mainLayout"] = new Template("Template.mainLayout", (function() {
-  var view = this;
-  return Spacebars.include(view.lookupTemplate("yield"));
-}));
-
-}).call(this);
